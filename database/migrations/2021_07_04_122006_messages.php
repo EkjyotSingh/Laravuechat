6e29da30cdb49a98702f7ac4f1e221125cfe82f3
EkjyotSingh <essitco.php11@gmail.com>
2021-07-04 17:55:36 +0530

    migrations uploaded

---
+++ database/migrations/2021_07_04_122006_messages.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Messages extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('from')->comment('Own Id'); // Own Id
+            $table->unsignedBigInteger('to')->comment('Other Id'); // Other Id
+            $table->boolean('read')->default(false);
+            $table->text('message');
+            $table->boolean('type');
+            $table->string('profile_image')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
